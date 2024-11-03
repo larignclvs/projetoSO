08b81d994592d5c3e15a2126897aaa4cf2188ae0
alexiasuares <99414867+alexiasuares@users.noreply.github.com>
2024-11-03 12:57:49 -0300

    FIFO

--- FIFO.py
+++ FIFO.py
@@ -1,26 +1,27 @@
-# FIFO
+memory = []  # lista onde armazenaremos as páginas na memória
+hits = 0  # contador de acessos onde a página já estava na memória
+misses = 0  # contador de acessos onde a página não estava na memória
 
-memory = [] #lista onde armazenaremos as páginas na memória
-hits = 0 #contador de acessos onde a pagina já estava na memória
-misses = 0 #contadir de acessos onde a página nãoo estava na memória
-memory_size = int(input("Tamanho da memória: ")) #tamanho da memória física
-page_sequence = input("Sequência de páginas: ").split(' ') #recebe sequência de acesso
-page_sequence = [page.strip() for page in page_sequence if page.strip()]
+memory_size = int(input("Tamanho da memória: "))  # tamanho da memória física
+page_sequence = input("Sequência de páginas : ").split(',')  # recebe sequência de acesso separada por vírgulas
 
-for page in page_sequence: #lê a sequência de páginas 
-    print(f"{page} |", end=" ")
-
-    if page in memory: 
-        print("Hit") # se já estiver na memória, um hit é incrementado
+for page in page_sequence:
+    print(f"page: {page}")
+    if page in memory:
         hits += 1
+        status = "hit"
     else:
-        print("Miss")
-        misses += 1 # senão, um miss é incrementado
-        
+        misses += 1
+        status = "miss"
         if len(memory) >= memory_size:
-            memory.pop(0)  #se o tamanho limite da memória tiver sido atingido, remove a página mais antiga.
-        memory.append(page)  
-        
-print("Total: ", (hits + misses))
-print("Hit rate:", hits, "/", (hits + misses))
-print("Miss rate:", misses, "/", (hits + misses))
+            memory.pop(0)  # se o tamanho limite da memória tiver sido atingido, remove a página mais antiga.
+        memory.append(page)
+    for p in memory:
+        print(f"[{p:^3}]")
+    for _ in range(memory_size - len(memory)):
+        print("[-1]")
+    print(f"\n[ {page} ] <- ({status})\n")
+
+total_accesses = hits + misses
+print(f"Hit rate ({hits}/{total_accesses}):")
+print(f"Miss rate ({misses}/{total_accesses}):")
